b32aa3a3859f55cbf043867cd83b0120962d3a91
agusb19 <agustinbrage19@gmail.com>
2023-12-21 10:59:04 -0300

    feat: export interface created in queries and types function

--- src/utils/generateTypes.js
+++ src/utils/generateTypes.js
@@ -1,6 +1,6 @@
 const generateTypeDefinition = (tableName, queries) => {
     
-    const typeInterface = `interface ${tableName}Interface {\n${Object.keys(queries).map(queryName => `  ${queryName}: string`).join('\n')}\n}\n`;
+    const typeInterface = `export interface ${tableName}Interface {\n${Object.keys(queries).map(queryName => `  ${queryName}: string`).join('\n')}\n}\n`;
 
     const typeDefinition = `export const ${tableName}Queries: ${tableName}Interface = ${JSON.stringify(queries, null, 2)};\n`;
 
