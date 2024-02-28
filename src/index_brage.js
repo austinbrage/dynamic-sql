e81fe5293ace1dcbeefca8f85a5b8284a192daf3
agusb19 <agustinbrage19@gmail.com>
2024-02-28 20:39:54 -0300

    feat: add new add-queries-brage method for all SQLs folders
    (find all SQL folders and generate the typescript objects on the same folders)

---
+++ src/index_brage.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+
+const { join, extname, basename, relative } = require('path');
+const processSQLFile = require('./utils/processSQL');
+const generateTypeDefinition = require('./utils/generateTypes');
+const { readdirSync, writeFileSync, statSync } = require('fs');
+
+// Initialize paths references from current working directory
+const currentDir = process.cwd();
+
+function isDirectory(path) {
+    return statSync(path).isDirectory();
+}
+
+function findSQLFolders(baseDir, folders = []) {
+    const entries = readdirSync(baseDir);
+    
+    if (entries.includes('sql')) {
+        const sqlFolder = relative(currentDir, join(baseDir, 'sql'));
+        if (!folders.includes(sqlFolder)) {
+            folders.push(sqlFolder);
+        }
+    }
+
+    for (const entry of entries) {
+        const entryPath = join(baseDir, entry);
+        if (isDirectory(entryPath)) {
+            findSQLFolders(entryPath, folders);
+        }
+    }
+}
+
+function generateQueries() {
+    const sqlFolders = [];
+    findSQLFolders(currentDir, sqlFolders);
+
+    if (sqlFolders.length === 0) {
+        return console.error('Could not find any "sql" folders in the project directory, please add them to your project');
+    }
+
+    for (const sqlFolder of sqlFolders) {
+        const sqlFolderDir = join(currentDir, sqlFolder);
+        const sqlFiles = readdirSync(sqlFolderDir).filter((fileName) => fileName.endsWith('.sql'));
+
+        if (sqlFiles.length === 0) {
+            console.warn(`No ".sql" files found in the "${sqlFolder}" directory`);
+            continue;
+        }
+
+        for (const fileName of sqlFiles) {
+            const filePath = join(sqlFolderDir, fileName);
+            const fileData = processSQLFile(filePath);
+
+            const tableName = basename(fileName, extname(fileName));
+            const outputFileName = join(sqlFolderDir, `${tableName}.ts`);
+            const typeDefinition = generateTypeDefinition(tableName, fileData);
+
+            writeFileSync(outputFileName, typeDefinition, 'utf-8');
+            console.log(`Query for ${tableName} in ${sqlFolder} added successfully`);
+        }
+    }
+}
+
+generateQueries();
